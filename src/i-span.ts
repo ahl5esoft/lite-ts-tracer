42e2a41fc44cac2dd032cfa52f068b81185a3e1e
连建伟 <3478289084@qq.com>
2023-04-25 12:02:17 +0800

    feat: 删除无用函数和文件

--- src/i-span.ts
+++ src/i-span.ts
@@ -1,24 +1,7 @@
-import { ITracerSpanContext } from './i-span-context';
-import { ITracer } from './i-tracer';
-
 export interface ITracerSpan {
-    addTags(keyValueMap: { [key: string]: any; }): ITracerSpan;
-
-    context(): ITracerSpanContext;
-
     finish(finishTime?: number): void;
-
-    getBaggageItem(key: string): string;
 
     log(keyValuePairs: { [key: string]: any; }, timestamp?: number): ITracerSpan;
 
-    logEvent(eventName: string, payload: any): void;
-
-    setBaggageItem(key: string, value: string): ITracerSpan;
-
-    setOperationName(name: string): ITracerSpan;
-
     setTag(key: string, value: any): ITracerSpan;
-
-    tracer(): ITracer;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * from './global';
 export * from './i-span';
 export * from './i-span-context';
 export * from './i-traceable';
