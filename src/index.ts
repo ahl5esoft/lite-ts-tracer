fb00d573f056f38da352db0edd622bcf81ec390b
连建伟 <3478289084@qq.com>
2023-04-25 12:06:21 +0800

    feat: ITracer->TracerBase

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './i-span';
 export * from './i-span-context';
 export * from './i-traceable';
-export * from './i-tracer';
 export * from './span-options';
+export * from './tracer-base';
 export * from './tracer-strategy';
